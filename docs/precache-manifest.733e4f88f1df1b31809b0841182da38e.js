a7901c901c68ced4d71b391363e4bcf839408e8d
petter <petter0915@gmail.com>
2022-07-25 21:29:38 -0500

    Add docs al repositorio

---
+++ docs/precache-manifest.733e4f88f1df1b31809b0841182da38e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "023c5ce73996a642a646",
+    "url": "/static/css/main.375feb0c.chunk.css"
+  },
+  {
+    "revision": "023c5ce73996a642a646",
+    "url": "/static/js/main.023c5ce7.chunk.js"
+  },
+  {
+    "revision": "8ab1872ef407a50de177",
+    "url": "/static/js/1.8ab1872e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c8a06b4fe24e60c2c4e71055e02acb9",
+    "url": "/index.html"
+  }
+];
